6a6f926e8608b1ee854c65729a0351026a565c11
Nicholas Lim <nicholas_lim@bbsshack.club>
2025-10-31 17:30:00 +0800

    Edit files

--- firmware.ino
+++ firmware.ino
@@ -1,12 +1,19 @@
+// Onboard LEDs.
 const int LED1 = D0;
 const int LED2 = D1;
 const int LED3 = D2;
+
+// Onboard buttons.
 const int S1 = D3;
 const int S2 = D4;
 const int S3 = D5;
+
+// GPIO pins 2-4.
 const int IO2 = D8;
 const int IO3 = D10;
 const int IO4 = D9;
+
+// Microcontroller mode status.
 bool ControlIO2 = false;
 bool ControlIO3 = false;
 bool ControlIO4 = false;
@@ -21,6 +28,7 @@ void setup() {
 }
 
 void loop() {
+    // Statements to enable GPIO control mode if a key is pressed for more than 3 seconds.
     if (digitalRead(S1) == HIGH && ControlIO2 == false) {
         delay(3000);
         if (digitalRead(S1) == HIGH) {
@@ -48,7 +56,12 @@ void loop() {
             digitalWrite(LED3, LOW);
         }
     }
+    buttonControlIO();
+    userProgram();
+}
 
+// Function for controlling GPIO pins with buttons.
+void buttonControlIO() {
     if (digitalRead(S1) == HIGH && ControlIO2 == true) {
         pinMode(IO2, OUTPUT);
         digitalWrite(IO2, HIGH);
@@ -61,4 +74,14 @@ void loop() {
         pinMode(IO4, OUTPUT);
         digitalWrite(IO4, HIGH);
     }
+}
+
+// Users add their program here.
+void userProgram() {
+    // Sample program.
+    pinMode(IO2, OUTPUT);
+    delay(500);
+    digitalWrite(IO2, HIGH);
+    delay(500);
+    pinMode(IO2, LOW);
 }
